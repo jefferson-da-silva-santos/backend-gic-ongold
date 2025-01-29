184501373f6e5723e9ea9898ef372ed7f236b426
Jefferson Santos <jeffrrwpg678@gmail.com>
2025-01-29 15:18:57 -0300

    Arquivo do banco

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.get('/', (req, res) => {
-  res.status(200).send('Ok');
-})
+  res.status(200).json({msg: "OK"});
+});
+
 // Middleware de tratamento de erros
 app.use(errorHandler);
 
