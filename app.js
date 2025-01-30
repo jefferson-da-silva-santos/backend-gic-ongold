155b21bfe7cce6cc065fdb80eac8298ab99bb5d2
Jefferson Santos <jeffrrwpg678@gmail.com>
2025-01-30 14:40:13 -0300

    'Update'

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 import express from 'express';
 import 'dotenv/config';
-import errorHandler from './utils/errorHandler.js';
+// import errorHandler from './src/utils/errorHandler.js';
 import cors from 'cors';
 
 const port = 3000;
 const app = express();
 
-
 app.use(cors());
 app.use(express.json());
 app.get('/', (req, res) => {
-  res.status(200).json({msg: "OK"});
+  res.status(200).json({msg: "OK a API subiu"});
 });
 
 // Middleware de tratamento de erros
-app.use(errorHandler);
+// app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`A API subiu com sucesso na porta ${port}!!`);
